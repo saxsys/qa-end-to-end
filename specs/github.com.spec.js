da9edc4e1ebaaaeb3654ca615c3238f68b3f7129
Konstantin Novikov <konstantin.novikov@saxsys.de>
2018-09-27 15:14:57 +0200

    commenting 'browser.waitForAngularEnabled(false)' option

--- specs/github.com.spec.js
+++ specs/github.com.spec.js
@@ -2,6 +2,7 @@ const { browser, element, by } = require('protractor');
 
 describe('when navigating to github homepage', () => {
   it('then registration form should be displayed', () => {
+    // setting wait for angular to false because github is a non angular application
     browser.waitForAngularEnabled(false);
     browser.get('https://github.com');
     const signUpFormIsDisplayed = element(by.css(`.home-hero-signup`)).isDisplayed();
